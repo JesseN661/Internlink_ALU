a20d0b7cf7912fd16774883ebb460e9a9b182d10
Liata-Ornella <l.ornella@alustudent.com>
2026-03-08 20:33:00 +0200

    Cleanup: removed unwanted files

--- frontend/main.js
+++
@@ -1,8 +0,0 @@
-// main.js
-document.getElementById('studentBtn').addEventListener('click', () => {
-    window.location.href = 'login.html';
-});
-
-document.getElementById('smeBtn').addEventListener('click', () => {
-    window.location.href = 'login.html';
-});
